0778748fcf8e898e2d526b73f028081b419ad430
Eric Hazen <hazen@bu.edu>
2025-02-02 13:55:42 -0500

    Release Rev 1.0 software

--- Software/girl-scouts/rev1_main/main.c
+++ Software/girl-scouts/rev1_main/main.c
@@ -21,7 +21,9 @@
 #define max(a,b) ((a)>(b)?(a):(b))
 #define max3(a, b, c) ((a>b)?((a>c)?a:c):((b>c)?b:c))
 
-struct cRGB led[3];
+#define NUM_LED 3
+
+struct cRGB led[NUM_LED];
 struct cRGB temp;
 
 #define BRITE 100
@@ -29,11 +31,11 @@ struct cRGB temp;
 //
 // display options
 //
-#define NUM_OPTIONS 9
+#define NUM_OPTIONS 4
 void rainbow();
 void flicker();
 void flash();
-void fade( uint8_t r, uint8_t g, uint8_t b);
+void spin();
 
 // needed but not used
 ISR(PCINT0_vect) {
@@ -46,7 +48,7 @@ ISR(PCINT0_vect) {
 
 void all_off() {
   memset( led, 0, sizeof(led));
-    ws2812_setleds(led,3);
+    ws2812_setleds(led,NUM_LED);
 }
 
 
@@ -83,7 +85,7 @@ int main(void)
     if( (button+1) & 4)
       led[2].r = 128;
     
-    ws2812_setleds(led,3);
+    ws2812_setleds(led,NUM_LED);
     _delay_ms(200);
 
     all_off();
@@ -100,25 +102,24 @@ int main(void)
       flash();
       break;
     case 3:
-      fade( 128, 0, 0);
+      spin();
       break;
-    case 4:
-      fade( 0, 128, 0);
-      break;
-    case 5:
-      fade( 0, 0, 128);
-      break;
-    case 6:
-      flash( 128, 128, 0);
-      break;
-    case 7:
-      flash( 0, 128, 128);
-      break;
-    case 8:
-      flash( 128, 0, 128);
-      break;
+//    case 4:
+//      fade( 0, 128, 0);
+//      break;
+//    case 5:
+//      fade( 0, 0, 128);
+//      break;
     }
   }
+}
+
+
+// set LED n (0-2) to color from table in flash
+void set_led_table( int n, int c1) {
+  led[n].r = pgm_read_byte(&(rgb[c1][0]));
+  led[n].g = pgm_read_byte(&(rgb[c1][1]));
+  led[n].b = pgm_read_byte(&(rgb[c1][2]));
 }
 
 
@@ -131,20 +132,12 @@ void flicker() {
     int c2 = rand() % NUM_RGB_COLORS;
     int c3 = rand() % NUM_RGB_COLORS;
 
-      led[0].r = pgm_read_byte(&(rgb[c1][0]));
-      led[0].g = pgm_read_byte(&(rgb[c1][1]));
-      led[0].b = pgm_read_byte(&(rgb[c1][2]));
+    set_led_table( 0, c1);
+    set_led_table( 1, c2);
+    set_led_table( 2, c3);
 
-      led[1].r = pgm_read_byte(&(rgb[c2][0]));
-      led[1].g = pgm_read_byte(&(rgb[c2][1]));
-      led[1].b = pgm_read_byte(&(rgb[c2][2]));
-
-      led[2].r = pgm_read_byte(&(rgb[c3][0]));
-      led[2].g = pgm_read_byte(&(rgb[c3][1]));
-      led[2].b = pgm_read_byte(&(rgb[c3][2]));
-
-      ws2812_setleds(led,3);
-      _delay_ms(100);
+    ws2812_setleds(led,NUM_LED);
+    _delay_ms(100);
   }
 }
 
@@ -156,22 +149,11 @@ void rainbow() {
   for( int n=0; n<NUM_RAINBOW; n++) {
     for( int i=0; i<NUM_RGB_COLORS; i++) {
 
-      //      int i1 = ((i+(NUM_RGB_COLORS/3))%NUM_RGB_COLORS);
-      //      int i2 = ((i1+(NUM_RGB_COLORS/3))%NUM_RGB_COLORS);	
+      set_led_table( 0, i);
+      set_led_table( 1, i);
+      set_led_table( 2, i);
 
-      led[0].r = pgm_read_byte(&(rgb[i][0]));
-      led[0].g = pgm_read_byte(&(rgb[i][1]));
-      led[0].b = pgm_read_byte(&(rgb[i][2]));
-
-      led[1].r = pgm_read_byte(&(rgb[i][0]));
-      led[1].g = pgm_read_byte(&(rgb[i][1]));
-      led[1].b = pgm_read_byte(&(rgb[i][2]));
-
-      led[2].r = pgm_read_byte(&(rgb[i][0]));
-      led[2].g = pgm_read_byte(&(rgb[i][1]));
-      led[2].b = pgm_read_byte(&(rgb[i][2]));
-
-      ws2812_setleds(led,3);
+      ws2812_setleds(led,NUM_LED);
       _delay_ms(100);
     }
   }
@@ -179,47 +161,48 @@ void rainbow() {
 
 
 //------------------------------------------------------------
-#define NUM_FLASH 3
-void flash( int r, int g, int b) {
-  for( int d=1000; d>10; d /= 2) {
-    for( int i=0; i<NUM_FLASH; i++) {
-      memset( led, 0, sizeof(led));
-      led[i].r = r;
-      led[i].g = g;
-      led[i].b = b;
-      ws2812_setleds(led,3);
-      delay_10ms( d/10);
+#define FLASH_CYCLES 3
+#define FLASH_MAX_DELAY 300
+void flash() {
+  int c = 0;
+  for( int k=0; k<FLASH_CYCLES; k++) {
+    for( int d=FLASH_MAX_DELAY; d>10; d /= 2) {
+      for( int i=0; i<NUM_LED; i++) {
+	memset( led, 0, sizeof(led));
+	set_led_table( i, c);
+	ws2812_setleds(led,NUM_LED);
+	delay_10ms( d/10);
+	c = (c+1) % NUM_RGB_COLORS;
+      }
+      all_off();
     }
-    all_off();
+    for( int d=2; d<=FLASH_MAX_DELAY; d *= 2) {
+      for( int i=0; i<NUM_LED; i++) {
+	memset( led, 0, sizeof(led));
+	set_led_table( i, c);
+	ws2812_setleds(led,NUM_LED);
+	delay_10ms( d/10);
+	c = (c+1) % NUM_RGB_COLORS;
+      }
+      all_off();
+    }
   }
 }
     
-
-//------------------------------------------------------------
-void fade( uint8_t r, uint8_t g, uint8_t b) {
-  uint8_t m = max3( r, g, b);
-  for( int i=0; i<m; i++) {
-    for( int n=0; n<3; n++) {
-      if( r)
-	led[n].r = i;
-      if( g)
-	led[n].g = i;
-      if( b)
-	led[n].b = i;
-    }
-    ws2812_setleds(led,3);
-    _delay_ms(10);
-  }
-  for( int i=m; i>0; i--) {
-    for( int n=0; n<3; n++) {
-      if( r)
-	led[n].r = i;
-      if( g)
-	led[n].g = i;
-      if( b)
-	led[n].b = i;
-    }
-    ws2812_setleds(led,3);
-    _delay_ms(10);
+#define NUM_SPIN 15
+#define SPIN_DELAY 250
+void spin() {
+  int colr[NUM_LED];
+  for( int i=0; i<NUM_LED; i++)
+    colr[i] = NUM_RGB_COLORS * i / NUM_LED;
+  for( int i=0; i<NUM_SPIN; i++) {
+    for( int n=0; n<NUM_LED; n++)
+      set_led_table( n, colr[n]);
+    ws2812_setleds(led,NUM_LED);
+    delay_10ms( SPIN_DELAY/10);
+    int t = colr[0];
+    for( int i=0; i<NUM_LED-1; i++)
+      colr[i] = colr[i+1];
+    colr[NUM_LED-1] = t;
   }
 }
